b4763510bce4cce84916fad531523031fa9ac809
IYuliia <catnew2012@gmail.com>
2023-04-26 15:57:59 +0200

    made color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onBtnStart);
+refs.stopBtn.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+  intervalId = setInterval(changeBcgColor, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+}
+
+function changeBcgColor() {
+  const color = getRandomHexColor();
+  refs.body.style.background = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
