0ee0d055e99dccaba7f2154e668a066ae47e8a99
IYuliia <catnew2012@gmail.com>
2023-04-26 18:54:34 +0200

    update promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  submitBtn: document.querySelector('button[type="submit"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+refs.submitBtn.addEventListener('click', event => {
+  event.preventDefault();
+
+  const firstDelay = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
+
+  if (!firstDelay || !step || !amount) {
+    alert('Please fill in all the fields');
+    return;
+  }
+
+  for (let i = 1; i <= amount; i += 1) {
+    const delay = firstDelay + (i - 1) * step;
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
